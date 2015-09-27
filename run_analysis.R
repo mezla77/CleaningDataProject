ef5175332fa94702384e9fed9d30b545e5739d00
mezla77 <mezla77@users.noreply.github.com>
2015-09-27 18:13:43 +1000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 features <- read.table("./features.txt", header=FALSE)
 
 '  Get indices of means & standard deviation columns  '
-meanstdIndexes <- sort(c(grep("mean()", features$V2, fixed=TRUE, ignore.case = FALSE), grep("std()", features$V2, fixed=TRUE, ignore.case = FALSE)))
+meanstdIndexes <- sort(c(grep("mean", features$V2, fixed=TRUE, ignore.case = TRUE), grep("std", features$V2, fixed=TRUE, ignore.case = TRUE)))
 
 '  Read then combine mean & standard deviation columns of X data  '
 testX <- read.table("./test/X_test.txt", header=FALSE)
@@ -33,8 +33,8 @@ remove(features)
 
 '  Create a dataset that containes means for all features/columns
    grouped by Subject and Activity  '
-MEANS_by_SubjectActivity <- aggregate(fullXYSubject[,3:68], by=list(Subject=fullXYSubject$Subject, Activity=fullXYSubject$Activity), mean)
+MEANS_by_SubjectActivity <- aggregate(fullXYSubject[,-(1:2)], by=list(Subject=fullXYSubject$Subject, Activity=fullXYSubject$Activity), mean)
 MEANS_by_SubjectActivity <- MEANS_by_SubjectActivity[order(MEANS_by_SubjectActivity$Subject, MEANS_by_SubjectActivity$Activity),]
 
 '  Write the dataset of means to a text file  '
-write.table(MEANS_by_SubjectActivity, "MEANS_by_SubjectActivity.txt", row.names=FALSE, sep="\t")
+write.table(MEANS_by_SubjectActivity, "tidy_data.txt", row.names=FALSE, sep="\t")
